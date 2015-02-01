e147dc996a4a11eecf7606e113baa9e12cac4166
PhilippSpo <philipp.sporrer@planifica.de>
2015-02-01 14:11:26 +0100

    fix for dropdown icon and version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'philippspo:tree',
-  version: '0.0.4',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'folder structure style tree built for mongo collections',
   // URL to the Git repository containing the source code for this package.
